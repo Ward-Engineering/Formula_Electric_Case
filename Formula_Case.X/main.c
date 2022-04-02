0f2279c801b9d00681e3a3007a5fbb08ff953b6f
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-04-02 11:24:19 +0200

    Initial Commit

---
+++ Formula_Case.X/main.c
@@ -0,0 +1,71 @@
+/*
+ * File:   main.c
+ * Author: Ward
+ *
+ * Created on 15 maart 2022, 23:36
+ */
+
+
+#include <xc.h>
+/*
+ * Prototypes
+ */
+void __interrupt (high_priority) high_ISR(void);   //high priority interrupt routine
+void __interrupt (low_priority) low_ISR(void);  //low priority interrupt routine, not used in this example
+void initChip(void);
+
+
+
+/*
+ * Global Variables
+ */
+//volatile int  counter = 0;
+
+/*
+ * Interrupt Service Routines
+ */
+/********************************************************* 
+	Interrupt Handler
+**********************************************************/
+void __interrupt (high_priority) high_ISR(void)
+{
+    
+    
+}
+
+/*
+ * Functions
+ */
+ /*************************************************
+			Main
+**************************************************/
+void main(void)
+{
+    initChip();
+
+}
+
+/*************************************************
+			Initialize the CHIP
+**************************************************/
+void initChip(void)
+{
+	//CLK settings
+	OSCTUNE = 0x80; //3X PLL ratio mode selected
+	OSCCON = 0x70; //Switch to 16MHz HFINTOSC
+	OSCCON2 = 0x10; //Enable PLL, SOSC, PRI OSC drivers turned off
+	while(OSCCON2bits.PLLRDY != 1); //Wait for PLL lock
+	ACTCON = 0x90; //Enable active clock tuning for USB operation
+
+    LATA = 0x00; //Initial PORTA
+    TRISA = 0xFF; //Define PORTA as input
+    ADCON1 = 0x00; //AD voltage reference
+    ANSELA = 0b00000001; // define analog or digital
+    CM1CON0 = 0x00; //Turn off Comparator
+    LATB = 0x00; //Initial PORTB
+    TRISB = 0x00; //Define PORTB as output
+    LATC = 0x00; //Initial PORTC
+    TRISC = 0x00; //Define PORTC as output
+    
+    
+}
